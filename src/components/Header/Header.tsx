bbd0872c696aee597714dbd9a6bb95029ad19ea8
aungkaungmyat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-01-16 19:58:19 +0630

    [Complete] Header components & configuration project structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import React from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { MdDarkMode } from "react-icons/md";
+
+const Header = () => {
+  return (
+    <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
+      <div className="flex items-center w-full md:h-2/3">
+        <Link href="/" className="font-black text-tertiary-dark">
+          Hotel
+        </Link>
+        <ul className="flex items-center ml-5">
+          <li className="flex items-center">
+            <Link href="/auth">
+              <FaUserCircle className="cursor-pointer" />
+            </Link>
+          </li>
+          <li className="ml-2">
+            <MdDarkMode className="cursor-pointer" />
+          </li>
+        </ul>
+      </div>
+      <ul className="flex items-center justify-between w-full md:w-1/3 mt-4">
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href="/rooms">Rooms</Link>
+        </li>
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href="/">Contact</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#038C7F",
+        secondary: "#F2C641",
+        tertiary: {
+          dark: "#F27405",
+          light: "#F2C641",
+        },
+      },
+      fontFamily: {
+        poppins: ["var(--font-poppins)", ...fontFamily.sans],
       },
     },
   },
   plugins: [],
-}
+};
