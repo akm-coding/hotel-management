c3bd0d7c920e21269a4b411b2c144606450c6bce
aungkaungmyat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-01-18 19:46:57 +0630

    Create themeContext & theme provider

---
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import ThemeContext from "@/context/themeContext";
+import React, { useEffect, useState } from "react";
+
+const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const themeFromStorage: boolean =
+    typeof localStorage !== "undefined" && localStorage.getItem("hotel-theme")
+      ? JSON.parse(localStorage.getItem("hotel-theme")!)
+      : false;
+
+  const [darkTheme, setDarkTheme] = useState<boolean>(themeFromStorage);
+  const [renderComponent, setRenderComponent] = useState(false);
+
+  useEffect(() => {
+    setRenderComponent(true);
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
+      <div className={`${darkTheme ? "dark" : ""} min-h-screen`}>
+        <div className="dark:text-white dark:bg-black text-[#1E1E1E]">
+          {children}
+        </div>
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
